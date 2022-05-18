57d70d761793f72134c617d6bf627a6dfe14e41c
willy137 <102767039+willy137@users.noreply.github.com>
2022-05-18 02:12:39 -0300

    cambios

--- manga/javasc.js
+++ manga/javasc.js
@@ -1,11 +1,11 @@
 var cambiar=0;
+imagenes=new Array
+imagenes[0]="muestra/jujutsu-kaisen_3840x2160_xtrafondos.jpg"
+imagenes[1]="muestra/bleach.jpg"
+imagenes[2]="muestra/Fate.jpg"
+imagenes[3]="muestra/hyouka.jpg"
+imagenes[4]="muestra/steins gate.jpg"
 function CarruselS() {
-    imagenes=new Array
-        imagenes[0]="muestra/jujutsu-kaisen_3840x2160_xtrafondos.jpg"
-        imagenes[1]="muestra/bleach.jpg"
-        imagenes[2]="muestra/Fate.jpg"
-        imagenes[3]="muestra/hyouka.jpg"
-        imagenes[4]="muestra/steins gate.jpg"
     if (cambiar<4) {
         cambiar+=1;
     }else if(cambiar==4){
@@ -15,18 +15,20 @@ function CarruselS() {
     return true;
 }
 function CarruselA(){
-    imagene=new Array
-    imagene[0]="muestra/jujutsu-kaisen_3840x2160_xtrafondos.jpg"
-    imagene[1]="muestra/bleach.jpg"
-    imagene[2]="muestra/Fate.jpg"
-    imagene[3]="muestra/hyouka.jpg"
-    imagene[4]="muestra/steins gate.jpg"
     if (cambiar>0) {
         cambiar-=1;
     }else if(cambiar==0){
         cambiar=4;
     }
-    document.galeria.src=imagene[cambiar];
+    document.galeria.src=imagenes[cambiar];
     return true;
 }
-alert("hola2");
+var cant=0;
+function modoOscuro(){
+    document.body.style.background='white';
+    cant+=1;
+    if(cant==2){
+        document.body.style.background='#3a3535';
+        cant=0;
+    }
+}

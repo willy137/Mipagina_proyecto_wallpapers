e092d2a1d6d881b9bb065616188f18d6205c5d9a
willy137 <102767039+willy137@users.noreply.github.com>
2022-05-19 19:30:33 -0300

    Update modooscurojavaScript.js

--- modooscurojavaScript.js
+++ modooscurojavaScript.js
@@ -1,6 +1,6 @@
 var cant=0;
 function modoOscuro(){
-    document.body.style.background='white';
+    document.body.style.background='#a5ebe8';
     cant+=1;
     if(cant==2){
         document.body.style.background='#3a3535';
